1453ddb1e656e9b0c803b3eebcdd4d554aa94861
Nathan Krislock <nkrislock@niu.edu>
2017-11-20 15:23:02 -0600

    plotsoln now works for 2D and 3D solutions

--- private/plotsoln.m
+++ private/plotsoln.m
@@ -2,6 +2,7 @@ function plotsoln(X, Xref, Xorig, A)
 
 m = size(A, 1);
 n = size(X, 1) + m;
+r = size(X, 2);
 
 % Plot solution
 clf
@@ -19,20 +20,38 @@ end
 
 %%%%%%%% plotXsol subroutine %%%%%%%%
     function plotXsol(Xsol, titletext)
-        plot(Xorig(:,1), Xorig(:,2), 'ko')
+        if r==2
+            plot(Xorig(:,1), Xorig(:,2), 'ko')
+        elseif r==3
+            plot3(Xorig(:,1), Xorig(:,2), Xorig(:,3), 'ko')
+        end 
         hold on
         for ii=1:n-m
-            plot([Xsol(ii,1) Xorig(ii,1)], ...
-                [Xsol(ii,2) Xorig(ii,2)], 'k-');
+            if r==2
+                plot([Xsol(ii,1) Xorig(ii,1)], ...
+                    [Xsol(ii,2) Xorig(ii,2)], 'k-');
+            elseif r==3
+                plot3([Xsol(ii,1) Xorig(ii,1)], ...
+                    [Xsol(ii,2) Xorig(ii,2)], ...
+                    [Xsol(ii,3) Xorig(ii,3)], 'k-');
+            end
         end
-        plot(A(:,1), A(:,2), 'rs')
-        plot(Xsol(:,1), Xsol(:,2), 'k.')
-        plot(A(:,1), A(:,2), 'r.')
-        axis('equal')
-        axis([-0.6, 0.6, -0.6, 0.6])
+        if ~isempty(A)
+            if r==2
+                plot(A(:,1), A(:,2), 'rs')
+                plot(A(:,1), A(:,2), 'r.')
+            elseif r==3
+                plot3(A(:,1), A(:,2), A(:,3), 'rs')
+                plot3(A(:,1), A(:,2), A(:,3), 'r.')
+            end
+        end
+        if r==2
+            plot(Xsol(:,1), Xsol(:,2), 'k.')
+        elseif r==3
+            plot3(Xsol(:,1), Xsol(:,2), Xsol(:,3), 'k.')
+        end
         title(titletext)
         hold off
     end
 
 end
-
